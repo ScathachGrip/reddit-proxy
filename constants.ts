9bd028952e4afa7137aed0ad0ccd190a69dc0287
sinkaroid <bibsindra@gmail.com>
2023-02-01 03:21:16 +0700

    fix: re-opening new port

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ export default {
   "reddit": "https://www.reddit.com",
 
   useragent: `Cabul/v${pkg.version} (github.com/sinkaroid/cabul.git);`,
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 2003,
   log: "cabul.log",
   error: "cabul.error.log"
 
